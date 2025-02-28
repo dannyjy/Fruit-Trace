dc4f895d27398595d4ac5945be075a9f96327220
muslmasan <muslmasan3@gmail.com>
2025-02-28 08:25:15 +0200

    d

--- backend/Fruit_Detection_Project/fruit_tracer/core/ml_model/classification.py
+++ backend/Fruit_Detection_Project/fruit_tracer/core/ml_model/classification.py
@@ -3,13 +3,16 @@ import numpy as np
 import tensorflow as tf
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__)) 
-MODEL_PATH = os.path.join(BASE_DIR, "fruit_model.h5")
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__)) 
+# MODEL_PATH = os.path.join(BASE_DIR, "fruit_model.h5")
+# take the command when the model is ready 
 
+model = tf.keras.models.load_model("fruit_model.h5")
 class_names = ["Not Edible", "Edible"]
 
 def classify_fruit(img_array):
     """Predicts if a given fruit is edible or not"""
+
     img_array = cv2.resize(img_array, (64, 64))
     img_array = np.expand_dims(img_array, axis=0)
     img_array = img_array / 255.0
